120efd73d564da9d4aa7b7940f2fe908a51d0951
KhanhPV19 <pkhanh494@gmail.com>
2025-03-03 10:45:48 +0700

    Initial commit

---
+++ WebApplication1/MyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MyDBContext : DbContext
+{
+    public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Đọc Connection String từ appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("MyDBContext");
+
+// Cấu hình SQL Server với Entity Framework Core
+builder.Services.AddDbContext<MyDBContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
